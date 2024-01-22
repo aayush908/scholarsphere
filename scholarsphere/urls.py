f44130c0cf4bd9ebfb54844b5df3cd700e1d4681
Aayush kapil <aayushkapil100@gmail.com>
2024-01-22 15:18:59 +0530

    admin

--- scholarsphere/urls.py
+++ scholarsphere/urls.py
@@ -20,7 +20,7 @@ from dashboard import views as dash_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls , name = "admin"),
     path('', include('dashboard.urls')),
     path('register' , dash_views.register , name = "register"),
     path('login' , auth_views.LoginView.as_view(template_name = "dashboard/login.html"), name = "login"),
